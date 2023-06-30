8c255c666dacdc1a1e721b96d84008e62d2773f9
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-06-30 15:08:49 +0300

    Update schema.sql for JDBC

--- springSecurityTutorial/DATABASE/schema.sql
+++ springSecurityTutorial/DATABASE/schema.sql
@@ -1,2 +1,21 @@
-CREATE SCHEMA IF NOT EXISTS `jet_security`;
-USE DATABASE `jet_security`
+CREATE DATABASE IF NOT EXISTS `jwt_security`;
+USE `jwt_security`;
+
+CREATE TABLE user
+(
+    id        INT PRIMARY KEY AUTO_INCREMENT,
+    firstname VARCHAR(255) NOT NULL,
+    lastname  VARCHAR(255) NOT NULL,
+    email     VARCHAR(255) NOT NULL,
+    password  VARCHAR(255) NOT NULL,
+    role      VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE note
+(
+    id            INT PRIMARY KEY AUTO_INCREMENT,
+    last_modified DATETIME     NOT NULL,
+    content       VARCHAR(255) NOT NULL,
+    user_id       INT          NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user (id)
+);
