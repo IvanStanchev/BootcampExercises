0744b590381dba42f7c0d8e7894bb7f74e3fd14a
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-07-04 13:39:28 +0300

    Add RepositoryConfig

--- springSecurityTutorial/src/main/java/com/example/springsecuritytutorial/config/RepositoryConfig.java
+++ springSecurityTutorial/src/main/java/com/example/springsecuritytutorial/config/RepositoryConfig.java
@@ -1,23 +1,20 @@
-//package com.example.springsecuritytutorial.config;
-//
-//import com.example.springsecuritytutorial.user.UserDAO;
-//import com.example.springsecuritytutorial.user.UserRepository;
-//import com.example.springsecuritytutorial.user.UserRepositoryMySQL;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Profile;
-//
-//@Configuration
-//@ComponentScan(basePackages = "com.example.springsecuritytutorial.user")
-//public class RepositoryConfig {
-//
-//    private final UserDAO userDAO;
-//
-//    @Profile("mysql")
-//    @Bean
-//    public UserRepository userRepositoryMySQL(UserDAO userDAO) {
-//        return new UserRepositoryMySQL(userDAO);
-//    }
-//}
+package com.example.springsecuritytutorial.config;
+
+import com.example.springsecuritytutorial.user.UserRepository;
+import com.example.springsecuritytutorial.user.MySQLUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages = "com.example.springsecuritytutorial.user")
+public class RepositoryConfig {
+
+    private DataSource dataSource;
+    @Bean
+    @Primary
+    public UserRepository mySQLUserRepository() {
+        return new MySQLUserRepository(dataSource);
+    }
+}
